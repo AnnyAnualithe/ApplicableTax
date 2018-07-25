dd9d3f98e7a5a4b59d8165c8e31e17f1a60a43c8
Anny Anualithe <41342283+AnnyAnualithe@users.noreply.github.com>
2018-07-25 20:20:22 +0200

    Update TaxCarculation.java

--- src/taxcarculation/TaxCarculation.java
+++ src/taxcarculation/TaxCarculation.java
@@ -1,22 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package taxcarculation;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author HP
- */
+
 public class TaxCarculation {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         ArrayList<Carculate> list = new ArrayList<Carculate>();
