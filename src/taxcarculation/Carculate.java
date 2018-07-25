e7cacb63c55ef029bcd3a46de03dac817b7ccf51
Aime Irak <aimeirak10@gmail.com>
2018-07-25 03:53:33 +0100

    First commit

---
+++ src/taxcarculation/Carculate.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taxcarculation;
+
+/**
+ *
+ * @author HP
+ */
+public class Carculate {
+    int quantity = 0;
+    String productName = null;
+    float price=0.0f;
+    boolean isImported=false;
+    boolean isExmpted = false;
+    float taxSales =0.0f;
+    public void Carculate(int quantity,String productName,float price,boolean isImported,boolean isExmpted){
+        this.quantity = quantity;
+        this.productName = productName;
+        this.price = price;
+        this.isImported = isImported;
+        this.isExmpted = isExmpted;
+    }
+    public void CalculateSale()
+    {
+        float totalTax = 0.0f;
+        if(isImported)
+            totalTax = 0.05f;
+        if(!isExmpted)
+            totalTax = 0.1f;
+        if(isImported && !isExmpted)
+        totalTax = 0.15f;
+        
+        taxSales = totalTax * price;
+    }
+    public String toString()
+    {
+        float total = price * quantity;
+        return quantity + " " + productName + " at "+total;
+    }
+}
---
+++ src/taxcarculation/TaxCarculation.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taxcarculation;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class TaxCarculation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Carculate> list = new ArrayList<Carculate>();
+        int number=1;
+        while(true)
+        { 
+            Carculate cal = new Carculate();
+            System.out.println("Product"+number);
+            System.out.println("Enter The Product Name:");
+            String productName = input.next();
+            cal.productName = productName;
+            System.out.println("Enter The Product Quantity");
+            int quantity = input.nextInt();
+            cal.quantity = quantity;
+            System.out.println("Enter The Price:");
+            float pr = input.nextFloat();
+            cal.price = pr;
+            System.out.print("This Product Is It Imported[y/n]:");
+            String imp = input.next();
+            if(imp.toLowerCase().equals("y")){
+                cal.isImported=true;
+            }
+            System.out.print("This Product Is It Exmpted[y/n]:");
+            String exemp = input.next();
+            if(imp.toLowerCase().equals("y")){
+                cal.isExmpted=true;
+            }
+            cal.CalculateSale();
+            list.add(cal);
+            number++;
+            System.out.print("Do you Want To Continue To Shop[y/n]:");
+            String add = input.next();
+            if(add.toLowerCase().equals("n"))
+                break;
+        }
+        float totalPrice = 0.0f;
+        float salesTax = 0.0f;
+        for (int i = 0; i < list.size(); i++) {
+            Carculate cal = list.get(i);
+            totalPrice += cal.price;
+            salesTax += cal.taxSales;
+            System.out.println(cal);
+        }
+
+        System.out.println("Sales Taxes:" + salesTax);
+        System.out.println("Total: " + (totalPrice + totalPrice + salesTax));
+    }
+    
+}
